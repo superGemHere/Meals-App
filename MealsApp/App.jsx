9f6ce6e48b0fc350dc15a3d0253a65e747d812cc
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-25 16:27:55 +0200

    Update App.jsx

--- MealsApp/App.jsx
+++ MealsApp/App.jsx
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -43,6 +44,7 @@ export default function App() {
                 //   };
                 // }}
               />
+              <Stack.Screen name="MealDetail" component={MealDetailScreen}/>
           </Stack.Navigator>
       </NavigationContainer>
     </>
